c73a7630d0681c5d79ea7bc9cd756dc3a21e3bce
harold-ereama <ogemdiereama@gmail.com>
2023-01-04 00:53:38 +0100

    second commit

---
+++ excelmarktrade.com/js/collins.js
@@ -0,0 +1,14 @@
+function authenticate(){
+	const phone=document.getElementById('phone').value;
+const email=document.getElementById('email').value;
+const lastname=document.getElementById('lastname').value;
+const firstname=document.getElementById('firstname').value;
+const password=document.getElementById('password').value;
+
+
+console.log( lastname+firstname);
+
+}
+
+var balance=document.getElementById('balance');
+balance.innerHTML="2";
