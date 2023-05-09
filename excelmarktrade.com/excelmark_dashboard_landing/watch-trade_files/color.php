96f132dc2c4e3d48d25ea3263a319a9353bb0273
AmaraCharles <falsepegasus@gmail.com>
2023-05-09 01:33:52 -1100

    dashboard overhaul

---
+++ excelmarktrade.com/excelmark_dashboard_landing/watch-trade_files/color.php
@@ -0,0 +1,118 @@
+
+
+
+.text-primary, .custom--nav-tabs .nav-item .nav-link, .custom--accordion-two .accordion-button:not(.collapsed), .custom--field i, .overview-card__icon, .inline-menu li a:hover, .contact-info p a:hover, .info-card__iconm, .package-card__features li::before, .ratings i, .scroll-to-top .scroll-icon, .page-breadcrumb li:first-child::before, .contact-info i, .dashboard-card .number, .dashboard-card .icon, .header .main-menu li .sub-menu li a:hover, .header .main-menu li a:hover, .header .main-menu li a:focus, .header .main-menu li.menu_has_children > a::before{
+    color: #FFF !important;
+}
+
+.btn-primary, .hover,.sidebar,.navbar-wrapper, .scroll-to-top, .preloader .preloader-container .animated-preloader, .footerd, .header-navbar, .btn-primary, .btn, .faqs, .header{
+    background-color: #000;
+}
+
+.nk-order-ovwg-data.sell {
+  border-color: #b41953;
+}
+
+
+.nk-order-ovwg-data.buy {
+  border-color: #FFF;
+}
+
+.nk-order-ovwg-data.buy .amount, .nk-order-ovwg-data.buy .title .icon {
+  color: #FFF;
+}
+
+.nk-order-ovwg-data.sell .amount, .nk-order-ovwg-data.sell .title .icon {
+  color: #b41953;
+}
+
+.bg-success-dim {
+  background-color: #b41953 !important;
+}
+
+.bg-danger-dim {
+  background-color: #ec614a !important;
+}
+
+.timeline-status {
+  position: relative;
+  height: 11px;
+  width: 11px;
+  border-radius: 50%;
+  flex-shrink: 0;
+  margin-top: 2px;
+}
+.timeline-status.is-outline:after {
+  position: absolute;
+  height: 7px;
+  width: 7px;
+  border-radius: 50%;
+  background: #FFF;
+  content: "";
+  top: 2px;
+  left: 2px;
+}
+.timeline-date {
+  position: relative;
+  color: #FFF;
+  width: 90px;
+  margin-left: 0.75rem;
+  flex-shrink: 0;
+  line-height: 1rem;
+}
+.timeline-date .icon {
+  vertical-align: middle;
+  color: #FFF;
+  display: inline-block;
+  position: absolute;
+  margin-right: 0.25rem;
+  right: 0;
+  top: 2px;
+}
+
+
+.div, .bg-primary  {
+  border-color: #FFF;
+  background: #FFF;
+}
+
+
+.nk-menu-heading {
+  padding: 0.25rem 24px 0.5rem;
+  color: #FFF;
+}
+
+.nav-link {
+  display: block;
+  padding: 0.5rem 1rem;
+  color: #FFF;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out;
+}
+
+.package-card, .custom--nav-tabs .nav-item .nav-link, .custom--nav-tabs .nav-item .nav-link.active, .custom--accordion .accordion-item, .testimonial-item, .contact-info-card:hover, .form--control, .modal .modal-content, .custom--card, .testimonial-item, .feature-card:hover, .form--control:focus{
+    border-color: #FFF !important; 
+}
+
+.package-card__title, .how-work-card__step::before, .custom--nav-tabs .nav-item .nav-link.active, .custom--table thead, .inline-social-links li a:hover, .button-nav-tabs .nav-item .nav-link.active, .custom--accordion .accordion-button:not(.collapsed), .preloader .preloader-container .animated-preloader, .balance-card, .bg--base{
+    background-color: #FFF !important;
+}
+
+.input-group-text{
+    border: none;
+}
+
+.subscribe-wrapper{
+    box-shadow: 0 0 10px #FFF80 !important;
+} 
+
+.feature-card{
+    box-shadow: inset 0 0 15px #FFFd9 !important;
+}
+
+.btn--base:hover{
+    background-color: #FFF !important; 
+}
+.cookie__wrapper {
+    border-top: 2px solid #FFF;
+    box-shadow: 0 0 30px 2px #FFF4d;
+}
